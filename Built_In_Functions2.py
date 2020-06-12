a140dec80336ff81f350ea1401594f4a407a21b2
Robzabel <robzabel88@gmail.com>
2020-06-12 18:23:26 +0100

    Todays work 12/06/20

---
+++ Built_In_Functions2.py
@@ -0,0 +1,12 @@
+from random import *
+import sys
+import pyperclip
+
+
+pyperclip.copy('hello world')
+pyperclip.paste()
+
+
+print(randint(1,56))
+sys.exit()
+print("you wont see this in the termianal output")
---
+++ Defining_Functions.py
@@ -0,0 +1,36 @@
+def hello():
+    print('Hello')
+    print('HOWDY')
+    print('Wosson')
+
+hello()
+hello()
+
+def Hello(name):
+    print("Hello " + name)
+
+print("Please enter your name: ")
+name = input()
+
+Hello(name)
+
+
+
+def plusOne(number):
+    return number + 1
+newNumber = plusOne(5)
+print(newNumber)
+
+
+spam = print()
+if spam == None:
+    print("True")
+
+print("Hello")
+print("World")
+
+print("Hello", end='')
+print("World")
+
+
+print("Hello","World", "is", "it","me", sep='0-0')
