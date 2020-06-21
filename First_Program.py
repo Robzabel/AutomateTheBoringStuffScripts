94a199130fdbba2008bf51a8bb063bdb62931f2b
Robzabel <32398556+Robzabel@users.noreply.github.com>
2020-06-21 12:04:51 +0100

    ad

--- First_Program.py
+++
@@ -1,6 +0,0 @@
-# this program is going to calculate the length of your name and ask your age
-
-MyName = input("What is your name? \nplease enter your name: ")     # this line accepts the name
-MyAge = input("How old are you?, please enter your age: ")          # this line accepts the age
-print("\nYour name is " + MyName + " which is " + str(len(MyName)) +    # this line prints the details
-      " characters long and you are " + MyAge + " years old")
