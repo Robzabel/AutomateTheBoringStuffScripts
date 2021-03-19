6cdb2ca3c055d281d2e967448d1f61131bcbde0c
Rob Zabel <32398556+Robzabel@users.noreply.github.com>
2021-03-19 10:57:00 +0000

    Update 40.Sending_emails.py

--- 40.Sending_emails.py
+++ 40.Sending_emails.py
@@ -3,8 +3,8 @@ connectionObject = smtplib.SMTP('smtp.gmail.com', 587) #create a connection obje
 print(type(connectionObject)) #see that the the connection object belongs to the SMTP class
 connectionObject.ehlo() #this is the function that starts the connection
 connectionObject.starttls() # start the tls protocol for password encryption
-connectionObject.login('robertozabelio31@gmail.com', 'Capital123!')#login to the gmail server
-connectionObject.sendmail('robertozabelio31@gmail.com','robzabel88@gmail.com', 'subject: test1\n\nThis is the third test')
+connectionObject.login('**********@gmail.com', '**********')#login to the gmail server
+connectionObject.sendmail('***********@gmail.com','*********@gmail.com', 'subject: test1\n\nThis is the third test')
 """in the line above is how you need to send the email, source address first then destination then headers and message
  the source email is not important becasue modern servers take it from the log in but it needs to be there or the 
  other parts are out of argumental position"""
